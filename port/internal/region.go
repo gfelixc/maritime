08bfaf1d03ddd09bcebf96acdf5477eac130214a
George Felix <gfelixc@gmail.com>
2023-04-10 20:42:07 +0200

    Modeling region

---
+++ port/internal/region.go
@@ -0,0 +1,17 @@
+package internal
+
+type Region struct {
+	value string
+}
+
+func NewRegion(value string) Region {
+	return Region{
+		value: value,
+	}
+}
+
+func (vo Region) String() string {
+	return vo.value
+}
+
+type RegionCollection []Region

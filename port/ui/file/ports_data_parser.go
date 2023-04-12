c96861084d3305d3f4e3457e66adb0ceb7e5e3a7
George Felix <gfelixc@gmail.com>
2023-04-12 02:43:22 +0200

    Added ports data parser

---
+++ port/ui/file/ports_data_parser.go
@@ -0,0 +1,79 @@
+package file
+
+import (
+	"bufio"
+	"encoding/json"
+	"io"
+)
+
+type PortsDataParser struct {
+	source *bufio.Reader
+}
+
+func NewPortsDataParser(source io.Reader) *PortsDataParser {
+	return &PortsDataParser{source: bufio.NewReader(source)}
+}
+
+func (p *PortsDataParser) NextPort() (Port, error) {
+	fieldName, err := p.getFieldName()
+	if err != nil {
+		return Port{}, err
+	}
+
+	object, err := p.getObject()
+	if err != nil {
+		return Port{}, err
+	}
+
+	port := Port{}
+	err = json.Unmarshal([]byte(object), &port)
+	if err != nil {
+		panic(err)
+	}
+
+	port.FieldName = fieldName
+
+	return port, err
+}
+
+func (p *PortsDataParser) getFieldName() (string, error) {
+	_, err := p.source.ReadString('"')
+	if err != nil {
+		return "", err
+	}
+
+	id, err := p.source.ReadString('"')
+	if err != nil {
+		return "", err
+	}
+
+	return id[:len(id)-1], nil
+}
+
+func (p *PortsDataParser) getObject() (string, error) {
+	_, err := p.source.ReadString('{')
+	if err != nil {
+		return "", err
+	}
+
+	chunk, err := p.source.ReadString('}')
+	if err != nil {
+		return "", err
+	}
+
+	return "{" + chunk, nil
+}
+
+type Port struct {
+	FieldName   string
+	Name        string
+	City        string
+	Country     string
+	Alias       []string
+	Regions     []string
+	Coordinates []float64
+	Province    string
+	Timezone    string
+	Unlocs      []string
+	Code        *string
+}
---
+++ port/ui/file/ports_data_parser_test.go
@@ -0,0 +1,82 @@
+package file
+
+import (
+	"io"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestPortsDataParser(t *testing.T) {
+	reader := strings.NewReader(inMemoryPortsData)
+
+	parser := NewPortsDataParser(reader)
+
+	t.Run("Should parse AEAJM properly", func(t *testing.T) {
+		port, err := parser.NextPort()
+
+		require.NoError(t, err)
+		require.Equal(t, "AEAJM", port.FieldName)
+		require.Equal(t, "Ajman", port.Name)
+		require.Equal(t, "Ajman", port.City)
+		require.Equal(t, "United Arab Emirates", port.Country)
+		require.Equal(t, []string{}, port.Alias)
+		require.Equal(t, []string{}, port.Regions)
+		require.Equal(t, []float64{55.5136433, 25.4052165}, port.Coordinates)
+		require.Equal(t, "Ajman", port.Province)
+		require.Equal(t, "Asia/Dubai", port.Timezone)
+		require.Equal(t, []string{"AEAJM"}, port.Unlocs)
+		require.Equal(t, "52000", *port.Code)
+	})
+
+	t.Run("Should parse missing Code from AEAUH as nil", func(t *testing.T) {
+		port, err := parser.NextPort()
+
+		require.NoError(t, err)
+		require.Equal(t, "AEAUH", port.FieldName)
+		require.Equal(t, (*string)(nil), port.Code)
+	})
+
+	t.Run("Should return io.EOF", func(t *testing.T) {
+		_, err := parser.NextPort()
+
+		require.ErrorIs(t, err, io.EOF)
+	})
+}
+
+var inMemoryPortsData = `{
+  "AEAJM": {
+    "name": "Ajman",
+    "city": "Ajman",
+    "country": "United Arab Emirates",
+    "alias": [],
+    "regions": [],
+    "coordinates": [
+      55.5136433,
+      25.4052165
+    ],
+    "province": "Ajman",
+    "timezone": "Asia/Dubai",
+    "unlocs": [
+      "AEAJM"
+    ],
+    "code": "52000"
+  },
+  "AEAUH": {
+    "name": "Abu Dhabi",
+    "coordinates": [
+      54.37,
+      24.47
+    ],
+    "city": "Abu Dhabi",
+    "province": "Abu Z¸aby [Abu Dhabi]",
+    "country": "United Arab Emirates",
+    "alias": [],
+    "regions": [],
+    "timezone": "Asia/Dubai",
+    "unlocs": [
+      "AEAUH"
+    ]
+  }
+}`
